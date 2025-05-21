444fae1e79352ac3ce53479436c10fd71f92b2c7
lebrancconvas <lebrancconvas@gmail.com>
2025-05-22 03:05:21 +0700

    ✨ feat: update template.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import "@/styles/index.css";
+
+export function App() {
+  return (
+    <>
+      <header>
+        <h1>Project Name</h1>
+      </header>
+      <section>
+
+      </section>
+      <footer>
+        <p>Made by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (@lebrancconvas)</a> in {new Date().getFullYear()}</p>
+      </footer>
+    </>
+  )
+}
+
