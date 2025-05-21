fb4fa18bed3da3e5f0751e58d3479767571a9c14
lebrancconvas <lebrancconvas@gmail.com>
2025-05-22 03:12:10 +0700

    ✨ feat: update template.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
+import { CONFIG } from "@/config";
 import "@/styles/index.css";
 
 export function App() {
   return (
     <>
       <header>
-        <h1>Project Name</h1>
+        <h1>{CONFIG.TITLE}</h1>
       </header>
       <section>
 
       </section>
       <footer>
-        <p>Made by <a href="https://github.com/lebrancconvas" target="_blank">Poom Yimyuean (@lebrancconvas)</a> in {new Date().getFullYear()}</p>
+        <p>Made by <a href={"https://github.com/" + CONFIG.USERNAME} target="_blank">{CONFIG.AUTHOR} (@{CONFIG.USERNAME})</a> in {CONFIG.YEAR}</p>
       </footer>
     </>
   )
-}
+};
 
---
+++ frontend/src/config/index.ts
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  TITLE: "Project Name",
+  AUTHOR: "Poom Yimyuean",
+  USERNAME: "lebrancconvas",
+  YEAR: new Date().getFullYear()
+};
